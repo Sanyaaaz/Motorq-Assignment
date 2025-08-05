42e1fe888246e1b0027be36823afb0f233dd824f
Sanya Saxena <sanyasaxena1465@gmail.com>
2025-08-05 15:24:41 +0530

    rethink

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sanya/connected_car_fleet/venv"
+set -gx VIRTUAL_ENV "/Users/sanya/Motorq Assignment/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
